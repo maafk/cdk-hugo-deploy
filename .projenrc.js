27a5535e0f3ff54a889f7c7463015a95313b2168
maafk <maafk@users.noreply.github.com>
2022-05-17 06:48:32 -0400

    Other tasks

--- .projenrc.js
+++ .projenrc.js
@@ -1,11 +1,20 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'maafk',
-  authorAddress: 'maafk@users.noreply.github.com',
+  author: 'Taylor Ondrey',
+  authorAddress: 'taylor@taylorondrey.com',
   cdkVersion: '2.24.1',
   defaultReleaseBranch: 'main',
   name: 'cdk-hugo-deploy',
+  description: 'Deploy Hugo static websites to AWS',
   repositoryUrl: 'https://github.com/maafk/cdk-hugo-deploy.git',
+  publishToPypi: {
+    distName: 'cdk-hugo-deploy',
+    module: 'cdk_hugo_deploy',
+  },
+  publishToNuget: {
+    packageId: 'maafk.CdkHugoDeploy',
+    dotNetNamespace: 'maafk.CdkHugoDeploy',
+  },
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
