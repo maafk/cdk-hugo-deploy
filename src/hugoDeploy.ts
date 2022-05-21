868ba36a3cab157f2c134cd296b38f9a69e13720
maafk <maafk@users.noreply.github.com>
2022-05-21 18:59:37 -0400

    inline Cloudfront function

--- src/hugoDeploy.ts
+++ src/hugoDeploy.ts
@@ -66,9 +66,17 @@ export class HugoDeploy extends Construct {
     });
 
     const cfFunction = new Function(this, 'HugoPaths', {
-      code: FunctionCode.fromFile({
-        filePath: join(dirname(realpathSync(__filename)), 'hugoPaths.js'),
-      }),
+      code: FunctionCode.fromInline(`function handler(event) {
+  var request = event.request;
+  var uri = request.uri;
+  if (uri.endsWith('/')) {
+    request.uri += 'index.html';
+  } else if (!uri.includes('.')) {
+    request.uri += '/index.html';
+  }
+  return request;
+}
+`),
     });
 
     const distribution = new Distribution(this, 'Distribution', {
